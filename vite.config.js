0fec810789c1c021bc806cf5b8db335167b9bd93
Yana Mironyak <yanika.mironyak@gmail.com>
2023-07-12 01:31:04 +0300

    fix: problem with deploy

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,13 @@ export default defineConfig(async () => {
   const glsl = await import("vite-plugin-glsl");
 
   return {
+    build: {
+      lib: {
+        entry: resolve(__dirname, "game.ts"),
+        name: "Game",
+        fileName: "game.ts",
+      },
+    },
     plugins: [glsl.default()],
     server: {
       host: true,
