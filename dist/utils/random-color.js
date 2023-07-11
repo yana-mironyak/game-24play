0a678445a4f14dff374c05a86cc6083565034edb
Yana Mironyak <yanika.mironyak@gmail.com>
2023-07-12 01:16:02 +0300

    fix: problem with deploy

--- dist/utils/random-color.js
+++
@@ -1,5 +0,0 @@
-export function getRandomColor() {
-  const colors = [0x0000ff, 0xffa500, 0x800080];
-  const randomIndex = Math.floor(Math.random() * colors.length);
-  return colors[randomIndex];
-}
